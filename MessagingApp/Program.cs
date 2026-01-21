af319dfd010a02cbc87f1aebc9ed00a43d8393e9
sa-es-ir <s.esmaeelinejad@gmail.com>
2026-01-21 21:29:56 +0000

    cleanup

--- MessagingApp/Program.cs
+++ MessagingApp/Program.cs
@@ -14,8 +14,8 @@ var aiEndPoint = builder.Configuration["AzureOpenAI:EndPoint"];
 var aiApiKey = builder.Configuration["AzureOpenAI:ApiKey"];
 
 builder.Services.AddSingleton(_ => new AzureOpenAIClient(new Uri(aiEndPoint!), new ApiKeyCredential(aiApiKey!)));
-builder.Services.AddSingleton<IConversationService, ConversationWithResponsesAPIService>();
-//builder.Services.AddSingleton<IConversationService, ConversationWithAssistantService>();
+//builder.Services.AddSingleton<IConversationService, ConversationWithResponsesAPIService>();
+builder.Services.AddSingleton<IConversationService, ConversationWithAssistantService>();
 builder.Services.Configure<AssistantOptions>(builder.Configuration.GetSection("AzureOpenAI"));
 
 
